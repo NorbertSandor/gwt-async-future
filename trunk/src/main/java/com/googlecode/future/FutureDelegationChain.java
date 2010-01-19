7a8da3d039f8fe998c7cbbd07184c511592e94f8
dean.povey <dean.povey@0de1a3fe-fb4c-11de-be8e-73f190f6c177>
2010-01-19 04:59:35 +0000

    Rename FutureDelegator -> FutureDelegationChain

--- trunk/src/main/java/com/googlecode/future/FutureDelegationChain.java
+++ trunk/src/main/java/com/googlecode/future/FutureDelegationChain.java
@@ -3,31 +3,33 @@ package com.googlecode.future;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
- * A future that evaluates a series of futures in sequence and completes when one
- * of them fufills the criteria of the {@link isFinished} method.  If this method
- * is not overriden then the default implementation checks for a non-null value.
+ * A future that evaluates a series of dependent futures in sequence until one
+ * of them satisfies the criteria of the {@link isFinished} method. If this
+ * method is not overriden then the default implementation checks for a non-null
+ * value.
  * 
- * <p>If not result satisfies the criteria the future will complete by calling 
- * setEmpty which will set the result to null.  Subclasses may override this method
- * to set a specific value.
+ * <p>
+ * If no result satisfies the criteria, the future will complete by calling
+ * setEmpty which will set the result to null. Subclasses may override this
+ * method to set a specific value.
  * 
  * @author Dean Povey
- *
- * @param <T> type to return
+ * 
+ * @param <T>
+ *            type to return
  */
-public class FutureDelegator<T> extends FutureAction<T> {
+public class FutureDelegationChain<T> extends FutureAction<T> {
     private final List<Future<T>> futures = new ArrayList<Future<T>>();
     private int nextFuture = 0;
     
-    public FutureDelegator(Future<T>...futures) {
+    public FutureDelegationChain(Future<T>...futures) {
         for (Future<T> future : futures) {
             this.futures.add(future);
         }
     }
     
-    public FutureDelegator(Iterable<Future<T>> futures) {
+    public FutureDelegationChain(Iterable<Future<T>> futures) {
         for (Future<T> future : futures) {
             this.futures.add(future);
         } 
@@ -45,6 +47,12 @@ public class FutureDelegator<T> extends FutureAction<T> {
         setEmpty();
     }
 
+    /**
+     * Called to see if the given future satisifes this request.
+     * 
+     * @param result
+     * @return
+     */
     public boolean isFinished(T result) {
         return result != null;
     }
--- trunk/src/test/java/com/googlecode/future/FutureDelegationChainTest.java
+++ trunk/src/test/java/com/googlecode/future/FutureDelegationChainTest.java
@@ -6,36 +6,36 @@ import static org.junit.Assert.*;
 import static com.googlecode.future.ConstantResult.*;
 
 
-public class FutureDelegatorTest {
+public class FutureDelegationChainTest {
     
     @SuppressWarnings("unchecked")
     @Test
     public void withNoDelegatesReturnsNull() {
-        FutureDelegator<Boolean> result = new FutureDelegator<Boolean>();
+        FutureDelegationChain<Boolean> result = new FutureDelegationChain<Boolean>();
         assertNull(result.get());
     }
     
     @SuppressWarnings("unchecked")
     @Test
     public void withSingleDelegateReturnsResult() {
-        FutureDelegator<Boolean> result = 
-            new FutureDelegator<Boolean>(constant(true));
+        FutureDelegationChain<Boolean> result = 
+            new FutureDelegationChain<Boolean>(constant(true));
         assertTrue(result.get());
     }
     
     @SuppressWarnings("unchecked")
     @Test
     public void withMultipleDelegatesReturnsFirstNonNullResult() {
-        FutureDelegator<Integer> result = 
-            new FutureDelegator<Integer>(constant((Integer)null), constant(1), constant(2));
+        FutureDelegationChain<Integer> result = 
+            new FutureDelegationChain<Integer>(constant((Integer)null), constant(1), constant(2));
         assertEquals(1, (int)result.get());
     }
     
     @SuppressWarnings("unchecked")
     @Test
     public void withCustomIsFinishedReturnsFirstValidResult() {
-        FutureDelegator<Integer> result = 
-            new FutureDelegator<Integer>(constant(1), constant(2), constant(3)) {
+        FutureDelegationChain<Integer> result = 
+            new FutureDelegationChain<Integer>(constant(1), constant(2), constant(3)) {
             @Override
             public boolean isFinished(Integer result) {
                 return result > 2; 
